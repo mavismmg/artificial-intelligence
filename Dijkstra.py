0a50957b16f58937f7ef5e4813718b95f4eea451
mavismmg <contatomateusjorgejf@gmail.com>
2023-05-29 21:47:33 -0300

    add: lower cost implementation

---
+++ Dijkstra.py
@@ -0,0 +1,28 @@
+import heapq
+
+def dijkstra(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    queue = [(0, start)]
+
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+        if current_distance > distances[current_node]:
+            continue
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(queue, (distance, neighbor))
+    return distances
+
+graph = {
+    'A': {'B': 5, 'C': 2},
+    'B': {'A': 5, 'C': 1, 'D': 3},
+    'C': {'A': 2, 'B': 1, 'D': 6},
+    'D': {'B': 3, 'C': 6}
+}
+start_node = 'A'
+
+distances = dijkstra(graph, start_node)
+print(distances)
